046539960e47059ab60bf3fb5d5ea36ba91c9faf
jaxlix <jax.lix@qq.com>
2020-01-14 18:12:23 +0800

    commit

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,28 @@
 <template>
 	<div id="index">
 		<div class="video-content">
-			<div v-for="(d,i) in videoList" :key="i" class="video-box" :class="'video-box-'+videoNum">
-				<video v-if="d.url" :src="d.url" poster="@/assets/icon/gloable-bg.png" autoplay controls class="video"></video>
+			<div v-for="(d,i) in playList" :key="i" class="video-box" :class="'video-box-'+videoNum">
+				<video v-if="d.type == 'video' && d.url" autoplay class="video">
+					<source :src="d.url" type="video/mp4">
+					Your browser does not support HTML5 video.
+				</video>
+				<div class="phone" v-if="d.type == 'audio' && d.url" >
+					<audio :src="d.url" autoplay></audio>
+				</div>
 			</div>
 		</div>
 		<div class="msg-list">
+			<div class="header">
+				<a class="v4" href="javascript:void(0)"></a>
+				<a class="v6" href="javascript:void(0)"></a>
+				<a class="v9" href="javascript:void(0)"></a>
+				<a class="v16" href="javascript:void(0)"></a>
+				<a class="to-right" href="javascript:void(0)"></a>
+				<div>
+					<a class="full-screen" href="javascript:void(0)"></a>
+					<a class="close" href="javascript:void(0)"></a>
+				</div>
+			</div>
 			<div class="btns">
 				<a href="javascript:void(0)">主会场模式</a>
 				<a href="javascript:void(0)">会商模式</a>
@@ -20,8 +37,8 @@
 						<p class="dept">{{d.deptment.name}}</p>
 					</div>
 					<a class="call" href="javascript:void(0)"></a>
-					<a class="call" href="javascript:void(0)"></a>
-					<a class="call" href="javascript:void(0)"></a>
+					<a class="end" href="javascript:void(0)"></a>
+					<a class="del" href="javascript:void(0)"></a>
 				</li>
 			</ul>
 		</div>
@@ -33,8 +50,120 @@
 		data() {
 			return {
 				videoNum: 9,
-				videoList: [],
+				playList: [],
 				magList: [
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
+					{
+						name: "张三",
+						no: 7894561,
+						deptment: {
+							name: "武汉市局"
+						}
+					},
 					{
 						name: "张三",
 						no: 7894561,
@@ -101,16 +230,20 @@
 			getList(){
 				let arr = [
 					{
-						url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+						type: "video",
+						url: "https://www.w3school.com.cn/i/movie.MP4"
 					},
 					{
-						url: "http://vjs.zencdn.net/v/oceans.mp4"
-					},
-					{
+						type: "video",
 						url: "https://media.w3.org/2010/05/sintel/trailer.mp4"
 					},
 					{
-						url: "http://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4"
+						type: "video",
+						url: "https://www.zhangxinxu.com/study/media/cat2.mp4"
+					},
+					{
+						type: "audio",
+						url: "https://www.w3school.com.cn/i/horse.ogg"
 					}
 				]
 				if(arr.length < this.videoNum){
@@ -122,14 +255,29 @@
 					arr = arr.splice(0, 9)
 				}
 				console.log(arr)
-				this.videoList = arr
+				this.playList = arr
 			}
 		},
 		created() {
 			this.getList()
 		},
 		mounted() {
-			
+			// let times = setInterval(() => {
+			// 	let loadAll = true
+			// 	for (const key in this.$refs) {
+			// 		if (this.$refs.hasOwnProperty(key) && key.indexOf("video") != -1) {
+			// 			const element = this.$refs[key];
+			// 			if(element.length > 0 && element[0].readyState == 4){
+			// 				element[0].play()
+			// 			}else{
+			// 				loadAll = false
+			// 			}
+			// 		}
+			// 	}
+			// 	if(loadAll){
+			// 		clearInterval(times)
+			// 	}
+			// }, 200);
 		}
 	};
 </script>
@@ -146,9 +294,17 @@
 			box-sizing: border-box;
 			border-right: 1px solid #000;
 			border-bottom: 1px solid #000;
+			background: url("../assets/icon/videoMeeting/bg.png") center center no-repeat;
 			.video{
 				width: 100%;
 				height: 100%;
+			}
+			.phone{
+				display: block;
+				width: 100%;
+				height: 100%;
+				background: url("../assets/icon/videoMeeting/bg_audio.png") center center no-repeat;
+				background-color: #2B3338;
 			}
 		}
 		.video-box-9{
@@ -170,7 +326,49 @@
 	}
 	.msg-list{
 		width: 410px;
+		height: 100%;
 		color: #fff;
+		overflow: hidden;
+		.header{
+			display: flex;
+			align-items: center;
+			height: 36px;
+			.v4{
+				margin: 10px;
+				width: 22px;
+				height: 15px;
+				background: url(../assets/icon/videoMeeting/4.png) center center no-repeat;
+				background-size: 100% 100%;
+			}
+			.v6{
+				margin: 10px;
+				width: 22px;
+				height: 15px;
+				background: url(../assets/icon/videoMeeting/6.png) center center no-repeat;
+				background-size: 100% 100%;
+			}
+			.v9{
+				margin: 10px;
+				width: 22px;
+				height: 15px;
+				background: url(../assets/icon/videoMeeting/9.png) center center no-repeat;
+				background-size: 100% 100%;
+			}
+			.v16{
+				margin: 10px;
+				width: 22px;
+				height: 15px;
+				background: url(../assets/icon/videoMeeting/16.png) center center no-repeat;
+				background-size: 100% 100%;
+			}
+			.to-right{
+				margin: 10px;
+				width: 22px;
+				height: 15px;
+				background: url(../assets/icon/videoMeeting/to_right.png) center center no-repeat;
+				background-size: 100% 100%;
+			}
+		}
 		.btns{
 			display: flex;
 			padding-left: 5px;
@@ -193,20 +391,63 @@
 		}
 		.msg-content{
 			padding: 10px;
+			height: calc(100% - 60px);
+			overflow-y: auto;
 			.msg-item{
 				display: flex;
-				margin-top: 10px;
+				margin-top: 15px;
 				.index{
 					width: 30px;
 				}
 				.msg{
 					flex: 1;
+					.dept{
+						font-size: 14px;
+						color: #ccc;
+					}
+				}
+				a{
+					margin: 5px;
 				}
 				.call{
-					width: 25px;
-					height: 26px;
+					width: 20px;
+					height: 21px;
 					background: url("../assets/icon/videoMeeting/call.png") center center no-repeat;
+					background-size: 100% 100%;
 				}
+				.end{
+					width: 30px;
+					height: 15px;
+					background: url("../assets/icon/videoMeeting/end.png") center center no-repeat;
+					background-size: 100% 100%;
+				}
+				.del{
+					width: 19px;
+					height: 20px;
+					background: url("../assets/icon/videoMeeting/del.png") center center no-repeat;
+					background-size: 100% 100%;
+				}
+			}
+			/*定义滚动条高宽及背景
+			高宽分别对应横竖滚动条的尺寸*/
+			&::-webkit-scrollbar {
+				width:10px;
+				height:16px;
+				background-color:#999;
+			}
+			/*定义滚动条轨道
+			内阴影+圆角*/
+			&::-webkit-scrollbar-track {
+				box-shadow: 0 0 6px rgba(0,0,0,0.3) inset;
+				border-radius: 5px;
+				background-color:#999;
+			}
+			/*定义滑块
+			内阴影+圆角*/
+			&::-webkit-scrollbar-thumb {
+				border-radius:10px;
+				box-shadow: 0 0 6px rgba(0,0,0,.3) inset;
+				background-color:#555;
 			}
 		}
 	}
